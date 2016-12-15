36ed43170ac62e72317709a070e95ebba0266fb4
palikrushev <palikrushev@gmail.com>
2016-12-15 09:29:55 +0100

    introduced error

--- 03_Testing/integratedSolution/filterClosestNPoints.m
+++ 03_Testing/integratedSolution/filterClosestNPoints.m
@@ -1,18 +1,21 @@
-function [ gridPositionsFiltered ] = filterClosestNPoints( gridPositions, distancesFromPoint, numberOfPointsToFilter )
+function [gridPositionsFiltered, distancesFromPointFiltered] = filterClosestNPoints(gridPositions, distancesFromPoint, numberOfPointsToFilter)
 
   if (numberOfPointsToFilter >= length(distancesFromPoint))
     gridPositionsFiltered = gridPositions;
+    distancesFromPointFiltered = distancesFromPoint;
     return;
   end
   
   [dimension,~] = size(gridPositions);
   
   gridPositionsFiltered = zeros(dimension, numberOfPointsToFilter);
+  distancesFromPointFiltered = zeros(numberOfPointsToFilter,1);
   
   for i=1:numberOfPointsToFilter
-    [~,index] = min(distancesFromPoint);
+    [value,index] = min(distancesFromPoint);
     distancesFromPoint(index) = Inf;
     gridPositionsFiltered(:,i) = gridPositions(:,index);
+    distancesFromPointFiltered(i,1) = value;
   end
 
 end
