afe0b2b8143c97e363a42648eef35d6364cbed30
palikrushev <palikrushev@gmail.com>
2016-12-24 04:27:36 +0100

    vars

---
+++ 03_Testing/integratedSolution/integratedTestGeneratorWGridAndPath.m
@@ -0,0 +1,57 @@
+function [totalError,totalConnectivity] = integratedTestGeneratorWGridAndPath(gridPositions,pathPositions)
+
+  numberOfPointsToFilter = 10;
+  radioRange = 0.5;
+  errorPercentageOfRange = 0.10;
+
+  pathPositionEstimates = pathPositions;
+  
+  [~,numberOfPathPoints] = size(pathPositions);
+  connectivity = zeros(numberOfPathPoints,1);
+  
+  for i = 1:numberOfPathPoints
+    currentPathPoint = pathPositions(:,i);
+    
+    distancesFromPoint = generateDistancesFromPoint(gridPositions,currentPathPoint);
+   
+    % introduce error
+    maximalError = radioRange * errorPercentageOfRange;
+    distancesFromPointWithError = introduceErrorToValues(distancesFromPoint, maximalError, 0, Inf);
+    % % %
+    
+    % filter N closest points
+    [gridPositionsFiltered, distancesFromPointFiltered] = filterClosestNPoints(gridPositions, distancesFromPointWithError, numberOfPointsToFilter);
+    % % %
+    
+    % filter distances larger than radioRange into INF
+    distancesFromPointWithRadioRange = turnLargerValuesIntoInf(distancesFromPointFiltered, radioRange);
+    % % %
+    
+    % calculate nonInfiniteDistances for connectivity
+    connectivity(i) = sum(distancesFromPointWithRadioRange ~= Inf);
+    % % %
+    
+    anchorDistanceMatrixFiltered = generateDistanceMatrix(gridPositionsFiltered); 
+    currentPathPointEstimate = integratedMdsMap(gridPositionsFiltered, anchorDistanceMatrixFiltered, distancesFromPointWithRadioRange);
+    
+    pathPositionEstimates(:,i) = currentPathPointEstimate;
+%    disp('--------');
+%    disp('current:');
+%    disp(currentPathPoint);
+%    disp('estimate:');
+%    disp(currentPathPointEstimate);
+%    disp('distance:');
+    disp(euclidDistance(currentPathPoint,currentPathPointEstimate));
+  end
+  
+  visualize(pathPositions, pathPositionEstimates);
+  
+  totalError = calculateTotalError(pathPositions, pathPositionEstimates, radioRange);
+  totalConnectivity = mean(connectivity);
+  disp('Total error:');
+  disp(totalError); 
+end
+
+function [distance] = euclidDistance(firstPoint,secondPoint)
+  distance = sqrt(sum((firstPoint-secondPoint).^2));
+end
